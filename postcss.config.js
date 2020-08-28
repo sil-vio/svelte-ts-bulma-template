b47a8d0f1757bb76cbe64422add13f47031f2e92
Silvio Giannini <giannini.silvio@gmail.com>
2020-08-28 12:30:13 +0200

    update package.json

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+const production = !process.env.ROLLUP_WATCH;
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+module.exports = {
+  plugins: [
+    require('postcss-import')(),
+    require('autoprefixer'),
+    production && purgecss({
+        content: ['./**/*.html', './**/*.svelte'],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      })
+  ]
+};
---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+
+<svelte:head>
+  <style src="style.scss"></style>
+</svelte:head>
+
+<main>
+	<section class="hero is-primary">
+		<div class="hero-body">
+		  <div class="container">
+			<h1 class="title">
+			  Hello {name}!
+			</h1>
+			<h2 class="subtitle">
+				Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.
+			</h2>
+		  </div>
+		</div>
+	  </section>
+</main>
